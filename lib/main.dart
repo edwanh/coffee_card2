85ffb9260a5a7b491fc4bec9d6e8b16f2b2cad1f
Edwan <edwanhajjaj@icloud.com>
2025-02-10 10:23:56 +0100

    Änderung

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Klickzähler'),
+          title: Text('Klicker'),
         ),
         body: CounterWidget(),
       ),
