3a6814f999cf5313fd79d8d47628139ea0106db8
Edwan <edwanhajjaj@icloud.com>
2025-02-10 10:44:05 +0100

    Änderung6

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('DrückDrück'),
+          title: Text('Clickcounter'),
         ),
         body: CounterWidget(),
       ),
