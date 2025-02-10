404a08847243c6bdd55ef7ff9c6a620db4ae3501
Edwan <edwanhajjaj@icloud.com>
2025-02-10 10:09:28 +0100

    test

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Klickzähler'),
+        ),
+        body: CounterWidget(),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Du hast so oft gedrückt:'),
+          Text(
+            '$_counter',
+            style: TextStyle(fontSize: 24),
+          ),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Text('Erhöhe den Zähler'),
+          ),
+        ],
+      ),
+    );
+  }
+}
