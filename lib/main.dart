9c72769e285234f037323bd4a990d4c2307d2a2c
Edwan <edwanhajjaj@icloud.com>
2025-02-20 14:58:30 +0100

    Aufgabe Variable/Konstanten

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,15 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Clickcounter'),
-        ),
-        body: CounterWidget(),
-      ),
-    );
-  }
-}
-
-class CounterWidget extends StatefulWidget {
-  @override
-  _CounterWidgetState createState() => _CounterWidgetState();
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Du hast so oft gedrückt:'),
-          Text(
-            '$_counter',
-            style: TextStyle(fontSize: 24),
-          ),
-          ElevatedButton(
-            onPressed: _incrementCounter,
-            child: Text('Erhöhe den Zähler'),
-          ),
-        ],
-      ),
-    );
-  }
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String dd = "!";
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String S = "Heute";
+  String spa = "";
 }
